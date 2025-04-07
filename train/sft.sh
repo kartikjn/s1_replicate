a98cf3c6538c4d94334cd5d05d45f2c91be35611
kartikjn <kj@rapidinnovation.dev>
2025-04-07 15:08:30 +0530

    Update fsdp_config and training script for improved configuration and performance

--- train/sft.sh
+++ train/sft.sh
@@ -10,16 +10,16 @@ micro_batch_size=1 # -> batch_size will be 16 if 16 gpus
 gradient_accumulation_steps=1 # requires more GPU memory
 max_steps=-1
 gpu_count=$(nvidia-smi -L | wc -l)
-push_to_hub=false
+push_to_hub=true
 
 torchrun --nproc-per-node ${gpu_count} --master_port 12345 \
     train/sft.py \
-    --block_size=32768 \
+    --block_size=8192 \
     --per_device_train_batch_size=${micro_batch_size} \
     --per_device_eval_batch_size=${micro_batch_size} \
     --gradient_accumulation_steps=${gradient_accumulation_steps} \
     --num_train_epochs=${epochs} \
-    --train_file_path="simplescaling/s1K_tokenized" \
+    --train_file_path="kj42/false_dataset_thinking_8k" \
     --model_name=${base_model} \
     --warmup_ratio=0.05 \
     --fsdp="full_shard auto_wrap" \
@@ -27,15 +27,16 @@ torchrun --nproc-per-node ${gpu_count} --master_port 12345 \
     --bf16=True \
     --eval_strategy="no" \
     --logging_steps=1 \
-    --save_strategy="no" \
+    --save_strategy="steps" \
+    --save_steps=50 \
     --lr_scheduler_type="cosine" \
     --learning_rate=${lr} \
     --weight_decay=${weight_decay} \
     --adam_beta1=0.9 \
     --adam_beta2=0.95 \
-    --output_dir="ckpts/s1-${uid}" \
+    --output_dir="kj42/s1_32b_jee-${uid}" \
     --push_to_hub=${push_to_hub} \
-    --save_only_model=True
-    # --gradient_checkpointing=True \ Enable gradient checkpointing for efficient memory usage with 8 H100 GPUs.
+    --save_only_model=True \
+    --gradient_checkpointing=True #\ Enable gradient checkpointing for efficient memory usage with 8 H100 GPUs.
     # --accelerator_config='{"gradient_accumulation_kwargs": {"sync_each_batch": true}}'
 
